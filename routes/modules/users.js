8148347a044360e1c44eb2b2736e6d8d405e3ab7
aaron1108 <wtfcaiii@gmail.com>
2023-04-08 20:10:19 +0800

    feat:add login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const { rawListeners } = require('../../models/todo')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.post('/login', (req, res) => {
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
